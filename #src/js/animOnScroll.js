15dc60b9388533f0bb4b70dc8e2a3da1fdc64573
Butterbroad <pyatochevskiy@gmail.com>
2020-09-28 01:00:39 +0300

    ie search

--- #src/js/animOnScroll.js
+++ #src/js/animOnScroll.js
@@ -41,5 +41,19 @@ if (animItems.length > 0) {
     }, 700);
 }
 
+// function msieversion() {
 
+//     var ua = window.navigator.userAgent;
+//     var msie = ua.indexOf("MSIE ");
+
+//     if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+//     {
+//       alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+//       alert('ie');
+//     } else {
+//       alert('ne ie');
+//     }
+//     return false;
+//   }
+//   msieversion();
 
--- LP-League/js/script.js
+++ LP-League/js/script.js
@@ -60,7 +60,21 @@ if (animItems.length > 0) {
     }, 700);
 }
 
+// function msieversion() {
 
+//     var ua = window.navigator.userAgent;
+//     var msie = ua.indexOf("MSIE ");
+
+//     if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+//     {
+//       alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+//       alert('ie');
+//     } else {
+//       alert('ne ie');
+//     }
+//     return false;
+//   }
+//   msieversion();
 
 
 function testWebP(callback) {
