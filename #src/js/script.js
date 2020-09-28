e44172900092de028bb7b0265d5f2fc78c3e5f58
imiasnikova_yulia <miasnikova.yulia@maxline.by>
2020-09-28 18:14:55 +0300

    started modal

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,18 +1,14 @@
+const infoBtn = document.querySelector('.info__learn');
+const modalInfo = document.querySelector('.modal-info');
+const modalInfoClose = document.querySelector('.modal-info__close');
 
+infoBtn.addEventListener('click', () => {
+  modalInfo.classList.add('active');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+modalInfoClose.addEventListener('click', () => {
+  modalInfo.classList.remove('active');
+});
 
 
 
