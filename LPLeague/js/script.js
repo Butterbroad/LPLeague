04689ab2856f91a63b26051bf48af599ac572e03
Butterbroad <pyatochevskiy@gmail.com>
2020-09-30 23:08:01 +0300

    finished lp

--- LPLeague/js/script.js
+++ LPLeague/js/script.js
@@ -2,13 +2,17 @@
 const infoBtn = document.querySelector('.info__learn');
 const modalInfo = document.querySelector('.modal-info');
 const modalInfoClose = document.querySelector('.modal-info__close');
+const footer = document.querySelector('.footer');
 
 infoBtn.addEventListener('click', () => {
   modalInfo.classList.add('active');
+
+  (window.innerWidth < 768) ? footer.style.display = 'none' : null;
 });
 
 modalInfoClose.addEventListener('click', () => {
   modalInfo.classList.remove('active');
+  (window.innerWidth < 768) ? footer.style.display = 'block' : null;
 });
 
 
